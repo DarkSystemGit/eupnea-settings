4ee7144637ed8088aec10963fce2092c1abdf4fa
darksystem <darksystemcoder@gmail.com>
2023-07-09 21:56:59 -0400

    devkit completed

--- devkit/main.js
+++ devkit/main.js
@@ -2,16 +2,24 @@ const fs = require('fs')
 const path = require('path')
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
+var args = processArgs(['readFile','outFile'])
 var dom = new JSDOM('<!DOCTYPE html><html><head id="head"></head><body id="__main"></body></html>')
 const document = dom.window.document
-var appFile= JSON.parse(fs.readFileSync(path.join(__dirname,'app.json')).toString())
+var appFile= JSON.parse(fs.readFileSync(path.join(__dirname,args.readFile+'.json')).toString())
 var componentList =  fs.readdirSync(path.join(__dirname,'/components'))
 var root = appFile.root
 var app ={}
 var xml = new JSDOM(fs.readFileSync(path.join(__dirname,'app.xml')))
-console.log(xml.window.document.getElementsByTagName('root'))
-function convertXmltoObj(name,obj){
+//console.log(xml.window.document.getElementsByTagName('root'))
+function processArgs(names){
 
+    var args ={}
+    for(var counter = 2;counter < process.argv.length;counter++){
+        //console.log(process.argv[counter])
+        args[names[counter-2]] = process.argv[counter]
+    }
+    //console.log(args)
+    return args
 }
 function convertObjectToDom(obj){
     if(componentList.includes(`${obj.type}.html`)){
@@ -42,4 +50,4 @@ function genChildren(parent){
 document.getElementById('__main').innerHTML =convertObjectToDom(root).replace('${__children}',genChildren(root))
 var docRoot = document.getElementById(root.id)
 //console.log(genChildren(root))
-fs.writeFileSync(path.join(__dirname,'page.html'),document.getElementsByTagName('html')[0].outerHTML)
+fs.writeFileSync(path.join(__dirname,args.outFile+".html"),document.getElementsByTagName('html')[0].outerHTML)
